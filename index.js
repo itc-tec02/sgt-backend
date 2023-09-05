743d1e99e7460e3e71aec744fc548c966191c8ff
itc-tec02 <tec02@it-consultores.com>
2023-09-05 18:01:36 -0500

    Priemra parte

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const app = require('./src/app')
 
 
-app.listen(3001)
+app.listen(3001, () => {
+    console.log("listening on port 3001");
+})
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require("express")
+
+const mainRouter = Router();
+
+module.exports = mainRouter;
+
+// app.get("/", (req, res) => {
+//     res.status(200).send("ok");
+// })
+
+// app.get("/users", (req, res) => {
+//     res.status(200).send("Estoy en users")
+// })
