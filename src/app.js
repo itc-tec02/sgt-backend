ae3e119f8fc2d40654496c50423732a288a8ef82
EdsonEvangelista <yokarc1989@gmail.com>
2023-12-01 10:57:37 -0500

    check token

--- src/app.js
+++ src/app.js
@@ -4,21 +4,11 @@ const mainRouter = require("./routes")
 const passport = require("passport");
 const cors = require("cors");
 const { SECRET_KEY } = process.env;
-// Create a JWT strategy
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 const app = express();
 
 app.use(morgan("dev"))
 app.use(express.json())
-
-const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: SECRET_KEY,
-};
-
-app.use(passport.initialize());
 
 app.use(cors());
 
